a6ca167838d7ac74ccf10353373f4ce65ead7894
Noah Forberich <noah@forberich.net>
2021-01-12 00:16:57 +0100

    Day 1 task 1

---
+++ Day1_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            foreach (string line in File.ReadAllLines("input.txt"))
+            {
+                numbers.Add(int.Parse(line));
+            }
+
+            foreach (int firstNumber in numbers)
+            {
+                foreach (int secondNumber in numbers)
+                {
+                    if (firstNumber + secondNumber == 2020)
+                    {
+                        Console.WriteLine(firstNumber * secondNumber);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
