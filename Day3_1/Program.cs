b0a15fc7d980887e91bc5d3fbdeec28883ca0f3f
Noah Forberich <noah@forberich.net>
2021-02-05 23:39:08 +0100

    Day 3 Task 1

---
+++ Day3_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Day3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int lineLength = input[0].Length;
+
+            int line = 0;
+            int index = 0;
+
+            int treeCount = 0;
+
+            while (true)
+            {
+                line++;
+                index += 3;
+                if (line == input.Length) break;
+                index = index % lineLength;
+                if (input[line][index] == '#')
+                {
+                    treeCount++;
+                }
+            }
+            Console.WriteLine($"The route contains {treeCount} Trees.");
+        }
+    }
+}
