dde34ff5fbc655eaf9899561c44d7a11fb286949
Noah Forberich <noah@forberich.net>
2021-02-06 13:22:25 +0100

    Day 6 Task 2

---
+++ Day6_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int personsIngroup = 0;
+            Dictionary<char, int> YesAnsweres = new Dictionary<char, int>();
+
+            int totalYesAnswers = 0;
+            foreach (string answers in input)
+            {
+                if (answers == String.Empty)
+                {
+                    foreach (KeyValuePair<char, int> answer in YesAnsweres)
+                    {
+                        if (answer.Value == personsIngroup) totalYesAnswers++;
+                    }
+                    YesAnsweres.Clear();
+                    personsIngroup = 0;
+                    continue;
+                }
+
+                foreach (char answer in answers)
+                {
+                    if (YesAnsweres.ContainsKey(answer) == false)
+                    {
+                        YesAnsweres.Add(answer, 1);
+                    }
+                    else
+                    {
+                        YesAnsweres[answer]++;
+                    }
+                }
+
+                personsIngroup++;
+            }
+
+            foreach (KeyValuePair<char, int> answer in YesAnsweres)
+            {
+                if (answer.Value == personsIngroup) totalYesAnswers++;
+            }
+
+            Console.WriteLine($"All groups answered yes to {totalYesAnswers} Yes.");
+        }
+    }
+}
