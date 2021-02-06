6cf4da4e459004aace019278c92197f363600e62
Noah Forberich <noah@forberich.net>
2021-02-06 13:20:44 +0100

    Day 6 Task 1

---
+++ Day6_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            HashSet<char> yesAnswers = new HashSet<char>();
+
+            int totalYesAnswers = 0;
+
+            foreach (string answers in input)
+            {
+                if (answers == String.Empty)
+                {
+                    totalYesAnswers += yesAnswers.Count;
+                    yesAnswers.Clear();
+                }
+
+                foreach (char answer in answers)
+                {
+                    yesAnswers.Add(answer);
+                }
+            }
+            totalYesAnswers += yesAnswers.Count;
+            yesAnswers.Clear();
+
+            Console.WriteLine($"All groups answered yes to {totalYesAnswers} Yes.");
+        }
+    }
+}
